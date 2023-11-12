886bfe66a029375105130d5bf4653c1355a42399
FPT-SHOP <FPT-SHOP@DESKTOP-GNJ2Q4V>
2023-11-12 14:33:59 +0700

    sound add

--- src/main/java/sound/Sound.java
+++ src/main/java/sound/Sound.java
@@ -1,6 +1,28 @@
 
 package Sound;
 
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class Sound {
-    
+    Clip clip;
+    URL[] soundURL = new URL[20];
+    public Sound(){
+        soundURL[0] = getClass().getResource("/playerSE/jump.wav"); 
+        soundURL[1] = getClass().getResource("/playerSE/bump.wav"); 
+        soundURL[2] = getClass().getResource("/playerSE/land.wav"); 
+        soundURL[3] = getClass().getResource("/playerSE/splat.wav"); 
+    }
+    public void playSE(int i){
+        try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+            clip.start();
+        }catch(Exception e){
+            
+        }
+    }
 }
